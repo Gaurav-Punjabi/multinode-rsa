06b3220589ab087332c704b7a44181c184d61635
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2024-11-15 22:23:20 -0800

    refactor: updated test.cpp to use include dirs

--- test.cpp
+++ test.cpp
@@ -1,54 +1,12 @@
 #include <iostream>
+#include <sstream>
 #include <cmath>
 #include <stdexcept>
+#include <omp.h>
+#include "includes/utils.h"
+#include "includes/key_generator.h"
 
 using namespace std;
-
-int compute_modulus(int p, int q) {
-  return p * q;
-}
-
-int compute_eulers_totient(int p, int q) {
-  return (p - 1) * (q - 1); 
-}
-
-int mod_inverse(int e, int eulers_totient) {
-    int t = 0, new_t = 1;
-    int r = eulers_totient, new_r = e;
-
-    while (new_r != 0) {
-        int quotient = r / new_r;
-        int temp_t = new_t;
-        new_t = t - quotient * new_t;
-        t = temp_t;
-
-        int temp_r = new_r;
-        new_r = r - quotient * new_r;
-        r = temp_r;
-    }
-
-    if (r > 1) throw invalid_argument("e is not invertible");
-    if (t < 0) t += eulers_totient;
-
-    return t;
-}
-
-int mod_exp(unsigned long long base, unsigned long long exp, unsigned long long mod) {
-    unsigned long long result = 1;
-    base = base % mod; // Handle large base values
-
-    while (exp > 0) {
-        // If exp is odd, multiply base with result
-        if (exp % 2 == 1) {
-            result = (result * base) % mod;
-        }
-        // Square the base and reduce exp by half
-        base = (base * base) % mod;
-        exp /= 2;
-    }
-
-    return (int)result;
-}
 
 int encrypt(int public_key, int e, int message) {
   return mod_exp(message, e, public_key);
@@ -59,23 +17,81 @@ int decrypt(int private_key, int public_key, int cipher) {
   return mod_exp(cipher, private_key, public_key);
 }
 
-
-
 int main() {
+  double start_time, end_time, encryption_time, decryption_time;
   int p = 61, q = 53;
   int e = 65537;
   int modulus = compute_modulus(p, q);
   int eulers_totient = compute_eulers_totient(p, q);
   int private_key = mod_inverse(e, eulers_totient);
-  cout << "\nThe private key is : " << private_key;
-  cout << "\nThe public key is : " << modulus;
-  int message = 89;
-  cout << "\nThe original message was : " << message;
-  long cipher = encrypt(modulus, e, message);
-  cout << "\nMessage after encryption : " << cipher;
+  size_t string_length = 99999999;
+  string input = generate_long_string(string_length);
 
-  long decrypted_message = decrypt(private_key, modulus, cipher);
+  string cipher_text = "";
 
-  cout << "\nThe decrypted text is : " << decrypted_message;
+  start_time = omp_get_wtime();
+
+  stringstream cipher_text_stream;
+
+    #pragma omp parallel
+    {
+        // Thread-local storage for each thread's result
+        stringstream local_cipher_text;
+
+        // Parallel for loop
+        #pragma omp for
+        for (int i = 0; i < input.length(); i++) {
+            int message = input[i];
+            int cipher = encrypt(modulus, e, message);
+
+            // Append the result to the thread's local stringstream
+            local_cipher_text << cipher << " ";
+        }
+
+        // After the loop, accumulate the local results into the global result
+        #pragma omp critical
+        {
+            cipher_text_stream << local_cipher_text.str();
+        }
+    }
+    cipher_text = cipher_text_stream.str();
+
+  // #pragma omp parallel for num_threads(4)
+  // for(int i=0;i<input.length();i++) {
+  //   int message = input[i];
+  //   int cipher = encrypt(modulus, e, message);
+  //   cipher_text.append(to_string(cipher));
+  //   cipher_text.append(" ");
+  // }
+
+  end_time = omp_get_wtime();
+
+  encryption_time = end_time - start_time;
+  cout << "\nTotal encryption time taken : " << encryption_time;
+
+
+  istringstream iss(cipher_text);
+
+  string decrypted_text = "";
+
+  int num;
+
+  start_time = omp_get_wtime();
+  while(iss >> num) {
+    int decrypted_message = decrypt(private_key, modulus, num);
+    decrypted_text.push_back((char)decrypted_message);
+  }
+  end_time = omp_get_wtime();
+  decryption_time = end_time - start_time;
+
+  cout << "\bTotal decryption time taken : " << decryption_time;
+
+  // cout << "\nThe original message was : " << message; 
+  // long cipher = encrypt(modulus, e, message);
+  // cout << "\nMessage after encryption : " << cipher;
+
+  // long decrypted_message = decrypt(private_key, modulus, cipher);
+
+  // cout << "\nThe decrypted text is : " << decrypted_message;
   return 0;
 }
